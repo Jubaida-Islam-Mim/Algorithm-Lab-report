c19027ed4d3ee89704340b4ae672662dd2f96f1d
Jubaida-Islam-Mim <jubaidaislammim7475@gmail.com>
2026-04-22 00:49:16 +0600

    Add files via upload

---
+++ Problem 4.5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_palindrome(char *str, int left, int right) {
+
+    if (left >= right) return 1;
+
+
+    if (str[left] != str[right]) return 0;
+
+
+    return is_palindrome(str, left + 1, right - 1);
+}
+
+int main() {
+    char s1[ ] = "racecar";
+    char s2[ ] = "hello";
+    char s3[ ] = "a";
+    char s4[ ] = "abba";
+
+    printf("is_palindrome(\"racecar\", 0, 6) => %d\n", is_palindrome(s1, 0, 6));
+    printf("is_palindrome(\"hello\", 0, 4) => %d\n", is_palindrome(s2, 0, 4));
+    printf("is_palindrome(\"a\", 0, 0) => %d\n", is_palindrome(s3, 0, 0));
+    printf("is_palindrome(\"abba\", 0, 3) => %d\n", is_palindrome(s4, 0, 3));
+
+    return 0;
+}
+
